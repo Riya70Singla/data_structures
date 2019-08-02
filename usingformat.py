1df879456dfcdbebbb89e3a8eaa6799615b3b87d
Riya70Singla <riya70singla@gmail.com>
2019-08-02 18:10:34 +0530

    format function in python

---
+++ usingformat.py
@@ -0,0 +1,6 @@
+name=input("Name: ")
+age=input("age: ")
+city=input("Current City: ")
+string="Your name is {} and you are {} years old.You live in {}."
+output=string.format(name,age,city)
+print(output)
