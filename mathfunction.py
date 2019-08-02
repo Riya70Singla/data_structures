15f5bbddc605de3d4f3f5a6fc929023b4a602b9b
Riya70Singla <riya70singla@gmail.com>
2019-08-02 17:45:14 +0530

    math functions in python

---
+++ mathfunction.py
@@ -0,0 +1,11 @@
+import math
+print(math.floor(1.5))
+print(math.ceil(2.1))
+print(math.cos(0))
+print(math.pi)
+print(math.hypot(12,5))
+print(math.pow(2,3))
+print(2 ** 3)
+print(math.exp(2))
+print(math.log2(8))
+
